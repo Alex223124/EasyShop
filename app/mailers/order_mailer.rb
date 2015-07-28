e05f0bee65cf069879af8250a9a174c915ba773a
alex223124 <dadeveloper@hotmail.com>
2015-07-28 21:43:09 +0000

    Orders Complete

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,15 @@
 class OrderMailer < ActionMailer::Base
-  default form: "blablabla@blablabla.net"
-  
+  default from: "tutsplus@josemota.net"
+
   def order_confirmation order
     @order = order
     mail to: order.user.email, subject: "Your order (##{order.id})"
+  end
+
+  def state_changed order, previous_state
+    @order = order
+    @previous_state = previous_state
+
+    mail to: order.user.email, subject: "Your order (##{order.id}) has changed!"
   end
 end
