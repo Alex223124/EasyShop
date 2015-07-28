73ac2cfb622c3b0d11faf2177f209f635c5a5d76
alex223124 <dadeveloper@hotmail.com>
2015-07-28 00:34:26 +0000

    Test pass

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-  mount_uploader :image, ImageUploader 
-  
+  mount_uploader :image, ImageUploader
+
   validates_presence_of :name, :price
-  validates_numericality_of :price 
+  validates_numericality_of :price
 end
